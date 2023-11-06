0dfb8029d8df66f63f1dba2ce7e51a050883afb3
Ace_Radom <sichenradomlyu@gmail.com>
2023-11-06 12:41:46 +0100

    add compile control macro

--- include/renatoolkits.h
+++ include/renatoolkits.h
@@ -3,7 +3,12 @@
 #ifndef _RENATOOLKITS_H_
 #define _RENATOOLKITS_H_
 
-#include"rich.h"
-#include"magic_enum_all.h"
+#ifndef __RENATOOLKITS_NO_RICH__
+#include"rich.h"                // in namespace rena::rich
+#endif
+
+#ifndef __RENATOOLKITS_NO_MAGIC_ENUM__
+#include"magic_enum_all.h"      // in namespace magic_enum
+#endif
 
 #endif
